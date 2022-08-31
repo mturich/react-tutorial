3bc83423e125d5bad98e8388dc6c339f99ccc5d5
Michael Urich <urich.michael@outlook.com>
2022-08-31 21:01:40 +0200

    finally all dev dependency issues

--- src/__tests__/Home.js
+++ src/__tests__/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import Home from '../Home.js'
+import { userEvent } from '@testing-library/user-event'
+import App from '../App.js'
+import * as useFetch from '../useFetch.js'
+
+jest.mock('../useFetch.js')
 
 test('this is the first test', () => {
-	render(<Home />)
-	//expect(screen.getByText(/Online shopping simplified/i)).toBeInTheDocument()
+	mockGet.mockResolvedValueOnce({value: 1})
+	const user = userEvent.setup()
+	render(<App />)
+	expect(screen.getByText(/Online shopping simplified/i)).toBeInTheDocument()
+	expect(screen.getByText(/Home/i)).toBeInTheDocument()
+	expect(screen.getByRole('link', {name: /Cart/i})).toBeInTheDocument()
+	const shopping = screen.getByRole('link', { name: /shopping/i })
+	
+	user.click(shopping)
+	expect(screen.getByText(/Online shopping simplified/i)).toBeInTheDocument()
+	expect(screen.getByText(/Home/i)).toBeInTheDocument()
+	expect(screen.getByRole('link', {name: /Cart/i})).toBeInTheDocument()
+
 })
