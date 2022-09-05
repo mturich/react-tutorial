9e4d2cac13c939207d763836a3733c8095ee1294
Michael Urich <urich.michael@outlook.com>
2022-09-05 12:01:43 +0200

    tests refactored

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, screen, act, waitFor} from '@testing-library/react'
+import {render, screen, act, waitFor, cleanup} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 //polyfill to the fetch api to run in an node env
@@ -7,9 +7,44 @@ import 'whatwg-fetch'
 // to handle a REST like post request
 import {setupServer} from 'msw/node'
 import {rest} from 'msw'
-import { loadStripe } from '@stripe/stripe-js'
+//import { loadStripe } from '@stripe/stripe-js'
 import App from '../App.js'
 import CART from '../shared/cart.js'
+import {isJSDocPropertyLikeTag} from 'typescript'
+
+//----------------------------------------------------------------
+/* 
+ jest.mock('@stripe/stripe-js', () => {
+   const untouchedStripe = jest.requireActual('@stripe/stripe-js')
+
+   jest.spyOn(untouchedStripe, 'loadStripe').mockImplementation(() => {
+      return Promise.resolve({
+         id: 'pk_test_51LYDlMGSORg56XufAuLQAcUuWYgqHjkZTSx0fLYs4EecMc6bXVtwd4qnkJEGKP1u7aXAi6I45t6f8IJRzoLtGiDk00ABSxWOKu',
+      })
+   })
+
+   return {...untouchedStripe}
+}) 
+
+//jest.mock('@stripe/stripe-js/pure')
+
+//jest.spyOn(mockLoadStripe, 'redirectToCheckout').mockResolvedValueOnce((props) => 42)
+
+ // Step 1.
+jest.mock("@stripe/stripe-js", () => {
+   const original = jest.requireActual("@stripe/stripe-js"); // Step 2.
+   return {
+       ...original,
+       mockLoadStripe: jest.fn()
+   };
+});
+
+// Step 4. Inside of your test suite:
+mockLoadStripe.mockResolvedValueOnce(() => {
+   return new Promise(resolve => resolve() )
+}); 
+ */
+//----------------------------------------------------------------
 
 const server = setupServer(
    // res - response, req - request, ctx - context
@@ -27,117 +62,124 @@ const server = setupServer(
    ),
 )
 
-beforeAll(() => server.listen({onUnhandledRequest: 'error'}))
-afterEach(() => server.resetHandlers())
+beforeAll(() => {
+   server.listen({onUnhandledRequest: 'error'})
+})
+
+afterEach(() => {
+   cleanup()
+   server.resetHandlers()
+})
+
 afterAll(() => {
    server.close()
    jest.clearAllMocks()
 })
 
-/* jest.mock('@stripe/stripe-js', () => {
-   const untouchedStripe = jest.requireActual('@stripe/stripe-js')
+test('1: if navbar is there', () => {
+   render(<App />)
+   expect(screen.getByRole('link', {name: /Home/i})).toBeInTheDocument()
+   expect(screen.getByRole('link', {name: /About us/i})).toBeInTheDocument()
+   expect(screen.getByRole('link', {name: /Products/i})).toBeInTheDocument()
+   expect(screen.getByRole('link', {name: /Cart/i})).toBeInTheDocument()
+})
 
-   jest.spyOn(untouchedStripe, 'loadStripe').mockImplementation(() => {
-      return Promise.resolve({
-         id: 'pk_test_51LYDlMGSORg56XufAuLQAcUuWYgqHjkZTSx0fLYs4EecMc6bXVtwd4qnkJEGKP1u7aXAi6I45t6f8IJRzoLtGiDk00ABSxWOKu',
-      })
-   })
+test('2: if home page is rendered correctly', () => {
+   render(<App />)
+   expect(screen.getByText(/Online shopping simplified/i)).toBeInTheDocument()
+   expect(
+      screen.getByRole('link', {name: 'Start shopping'}),
+   ).toBeInTheDocument()
+})
 
-   return {...untouchedStripe}
-}) */
+test('3: if Product Page is renderd correctly', async () => {
+   const user = userEvent.setup()
+   render(<App />)
 
-//jest.mock('@stripe/stripe-js/pure')
+   // Switch to Products
+   const shopping = screen.getByRole('link', {name: 'Start shopping'})
+   user.click(shopping)
+   expect(await screen.findByText(/Cheese/)).toBeInTheDocument()
+   expect(await screen.findByText(/200g cheese block/i)).toBeInTheDocument()
+})
 
-//jest.spyOn(mockLoadStripe, 'redirectToCheckout').mockResolvedValueOnce((props) => 42)
+test('4: if product function are working isJSDocPropertyLikeTag', async () => {
+   const user = userEvent.setup()
+   render(<App />)
 
-/* // Step 1.
-jest.mock("@stripe/stripe-js", () => {
-   const original = jest.requireActual("@stripe/stripe-js"); // Step 2.
-   return {
-       ...original,
-       mockLoadStripe: jest.fn()
-   };
-});
+   //search for AddProdToCart Button
+   const addCheeseToCart = await screen.findByRole('button', {name: /10/i})
+   expect(addCheeseToCart).toBeInTheDocument()
+   // add Cheese to the cart
+   user.click(addCheeseToCart)
+   expect(await screen.findByRole('link', {name: /(1)/i})).toBeInTheDocument()
 
-// Step 4. Inside of your test suite:
-mockLoadStripe.mockResolvedValueOnce(() => {
-   return new Promise(resolve => resolve() )
-}); */
+   // checks if Product can be deleted
+   const deleteProd = await screen.findByRole('button', {name: /x/i})
+   expect(deleteProd).toBeInTheDocument()
+   user.click(deleteProd)
+   expect(await screen.findByRole('link', {name: /(0)/i})).toBeInTheDocument()
 
-   test('tests the Product rendering by intercepting FETCH call', async () => {
-      //loadStripe.mockResolvedValueOnce(42)
-      const user = userEvent.setup()
-      render(<App />)
-      expect(screen.getByText(/Online shopping simplified/i)).toBeInTheDocument()
-      expect(screen.getByText(/Home/i)).toBeInTheDocument()
+   // add another Cheese to the cart
+   user.click(addCheeseToCart)
+   expect(await screen.findByRole('link', {name: /(1)/i})).toBeInTheDocument()
+   user.click(addCheeseToCart)
+   expect(await screen.findByRole('link', {name: /(2)/i})).toBeInTheDocument()
+})
 
-      const cart = screen.getByRole('link', { name: /Cart/i })
-      expect(cart).toBeInTheDocument()
+test('5: test ProductInfoPage', async () => {
+   const user = userEvent.setup()
+   render(<App />)
+   
+   // switch to ProductDetail Page
+   const prodDetailLink = await screen.findByRole('link', {name: /cheese/i})
+   user.click(prodDetailLink)
 
-      // Switch to Products
-      const shopping = screen.getByRole('link', { name: /shopping/i })
-      user.click(shopping)
-      expect(await screen.findByText(/Cheese/)).toBeInTheDocument()
-      expect(await screen.findByText(/200g cheese block/i)).toBeInTheDocument()
+   expect(await screen.findByText(/Details/)).toBeInTheDocument()
+   const detailLink = await screen.findByRole('link', {name: /Details/})
+   expect(detailLink).toBeInTheDocument()
+   expect(await screen.findByRole('button', {name: /10/i})).toBeInTheDocument()
 
-      //search for AddProdToCart Button
-      const addCheeseToCart = await screen.findByRole('button', { name: /10/i })
-      expect(addCheeseToCart).toBeInTheDocument()
-      // add Cheese to the cart
-      user.click(addCheeseToCart)
-      expect(await screen.findByRole('link', { name: /(1)/i })).toBeInTheDocument()
+   const nutritionLink = screen.getByRole('link', {name: /Nutrition/i})
+   expect(nutritionLink).toBeInTheDocument()
 
-      // checks if Product can be deleted
-      const deleteProd = await screen.findByRole('button', { name: /x/i })
-      expect(deleteProd).toBeInTheDocument()
-      user.click(deleteProd)
-      expect(await screen.findByRole('link', { name: /(0)/i })).toBeInTheDocument()
+   user.click(nutritionLink)
+   expect(await screen.findByRole('table')).toMatchSnapshot('nutrition')
+   user.click(detailLink)
 
-      // add another Cheese to the cart
-      user.click(addCheeseToCart)
-      expect(await screen.findByRole('link', { name: /(1)/i })).toBeInTheDocument()
-      user.click(addCheeseToCart)
-      expect(await screen.findByRole('link', { name: /(2)/i })).toBeInTheDocument()
+   user.click(await screen.findByRole('button', {name: /10/i}))
+   expect(await screen.findByRole('link', {name: /(3)/})).toBeInTheDocument()
 
-      // switch to ProductDetail Page
-      const prodDetailLink = screen.getByRole('link', { name: /cheese/i })
-      user.click(prodDetailLink)
+})
 
-      expect(await screen.findByText(/Details/)).toBeInTheDocument()
-      const detailLink = await screen.findByRole('link', { name: /Details/ })
-      expect(detailLink).toBeInTheDocument()
-      expect(await screen.findByRole('button', { name: /10/i })).toBeInTheDocument()
+test('6: tests if Cart component is working properly', async () => {
+   const user = userEvent.setup()
+   render(<App />)
 
-      const nutritionLink = screen.getByRole('link', { name: /Nutrition/i })
-      expect(nutritionLink).toBeInTheDocument()
+   const cart = screen.getByRole('link', {name: /Cart/i})
+   // Switch to Cart
+   user.click(cart)
+   expect(await screen.findByText('Your Cart')).toBeInTheDocument()
+   // check for chart table
+   expect(await screen.findByRole('table')).toMatchSnapshot('cart')
 
-      user.click(nutritionLink)
-      // something is needed to be checked. if the click backwards is done it fails
-      expect(await screen.findByRole('table')).toMatchSnapshot('nutrition')
-      user.click(detailLink)
+   //checks if
+   const email = screen.getByLabelText(/Email/i)
+   expect(email).toBeInTheDocument()
+   expect(email).toHaveTextContent('')
 
-      user.click(await screen.findByRole('button', { name: /10/i }))
-      expect(await screen.findByRole('link', { name: /(3)/ })).toBeInTheDocument()
-      //expect(await screen.findByRole('link', {name: /(3)/})).toBeInTheDocument()
+   await act(() => user.type(email, 'onlineshop1@outlook.de'))
+   expect(email).toHaveValue('onlineshop1@outlook.de')
 
-      // Switch to Cart
-      user.click(cart)
-      expect(await screen.findByText('Your Cart')).toBeInTheDocument()
-      // check for chart table
-      expect(await screen.findByRole('table')).toMatchSnapshot('cart')
+   const pay = screen.getByRole('button', {name: /Pay/i})
+   user.click(pay)
+})
 
-      //checks if
-      const email = screen.getByLabelText(/Email/i)
-      expect(email).toBeInTheDocument()
-      expect(email).toHaveTextContent('')
+test('7: if Stripe checkout works', () => {
+   const user = userEvent.setup()
+   render(<App />)
 
-      await act(() => user.type(email, 'onlineshop1@outlook.de'))
-      expect(email).toHaveValue('onlineshop1@outlook.de')
-
-      const pay = screen.getByRole('button', { name: /Pay/i })
-      user.click(pay)
-
-      //await waitFor(() => console.log(loadStripe.mock))
-      //await waitFor(() => expect(loadStripe).toBeCalledTimes(1))
-      //expect(mockLoadStripe).toHaveBeenCalledWith('paid with stripe')
-   });
+   //await waitFor(() => console.log(loadStripe.mock))
+   //await waitFor(() => expect(loadStripe).toBeCalledTimes(1))
+   //expect(mockLoadStripe).toHaveBeenCalledWith('paid with stripe')
+})
